c2fe9e92c18ec66ba8c2f80916769a3ae119a541
slort <fenrnadoslort@gmail.com>
2021-02-10 11:44:16 -0300

    published

--- App.js
+++ App.js
@@ -1,21 +1,54 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import "react-native-gesture-handler";
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import Carrusel from "./App/screens/Carrusel";
+import AppLoading from "expo-app-loading";
+import * as Fonts from "expo-font";
+
+const Stack = createStackNavigator();
 
 export default function App() {
+  const [load, setLoad] = useState(false);
+
+  const fetchFonts = Fonts.loadAsync({
+    robotoBlack: require("./assets/fonts/Roboto-Black.ttf"),
+    robotoBold: require("./assets/fonts/Roboto-Bold.ttf"),
+    robotoMedium: require("./assets/fonts/Roboto-Medium.ttf"),
+    robotoRegular: require("./assets/fonts/Roboto-Regular.ttf"),
+    robotoLigth: require("./assets/fonts/Roboto-Light.ttf"),
+  });
+
+  if (!load) {
+    return (
+      <AppLoading
+        startAsync={() => fetchFonts}
+        onFinish={() => setLoad(true)}
+        onError={(error) => console.log(error)}
+      />
+    );
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="carousel"
+          component={Carrusel}
+          options={{ headerShown: false }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
---
+++ App/constants/images.js
@@ -0,0 +1,9 @@
+export const image1 = require("../images/image1.png");
+export const image2 = require("../images/image2.png");
+export const image3 = require("../images/image3.png");
+
+export default {
+  image1,
+  image2,
+  image3,
+};
